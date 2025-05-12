6de481cfdd0e5396773423c3cbe609c6da153661
TedIvanac <Tedivanac@gmail.com>
2025-05-11 22:24:34 -0400

    Started system file, started ode solver

--- main_sim.py
+++ main_sim.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 start = [5,4]
 goal = [100,70]
@@ -9,4 +10,12 @@ Y = start(1) #starting y coordinate
 V = 8 #initial velocity
 int_e_v = 0
 int_e_s = 0
+init_state = np.array([X,Y,V,int_e_v,int_e_s])
 
+#Time parameters
+t_end = 60
+dt = 0.00001
+time = range(0,t_end,dt)
+
+#solve odes using runge-kutta 4,5
+[t,y] = scipy.integrate.solve_ivp(bicycle_system,time,init_state)
